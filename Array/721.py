bf6889758686b19710ac8d985acc4619e0ca9b87
root <joshuahu@tencent.com>
2021-01-18 11:23:51 +0800

    721 not finish, still not understand

---
+++ Array/721.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+
+        return 0
+
+
+if __name__ == "__main__":
+    asolute = Solution()
+    accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"],
+                ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+    asolute.accountsMerge(accounts)
